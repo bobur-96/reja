c77e5562b6d84181928554511e75a447d5b52429
bobur-96 <bovlogs96@gmail.com>
2024-08-24 19:16:32 +0900

    feat: mongoDB ulanish

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const res = require("express/lib/response");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// MongoDB connect
+
+// 1 Kirish code
+app.use(express.static("public")); // har qanday browserd kirib kelayotgan requestlar uchun public folder ochiq
+app.use(express.json()); // kirib kelayot json formatidagi data ni object holatiga ogirib beradi
+app.use(express.urlencoded({ extended: true })); // form dan qabul qilingan datalarni express qabul qiladi serverga kiritadi
+
+// 2: Session code
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing
+app.post("/create-item", (req, res) => {
+  console.log(req);
+  res.json({ test: "succes" });
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,45 +1,29 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
-const res = require("express/lib/response");
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://bobur9620:bobur9620$@cluster0.drq0e.mongodb.net/";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connecting MongoDB:");
+    else {
+      console.log("MongoDb connection succeed");
+      console.log(client);
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-// 1 Kirish code
-app.use(express.static("public")); // har qanday browserd kirib kelayotgan requestlar uchun public folder ochiq
-app.use(express.json()); // kirib kelayot json formatidagi data ni object holatiga ogirib beradi
-app.use(express.urlencoded({ extended: true })); // form dan qabul qilingan datalarni express qabul qiladi serverga kiritadi
-
-// 2: Session code
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing
-app.post("/create-item", (req, res) => {
-  console.log(req);
-  res.json({ test: "succes" });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
