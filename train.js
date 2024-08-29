af4e5f77804d85a8a79e56bff2a6eab54e98ff3c
bobur-96 <bovlogs96@gmail.com>
2024-08-29 23:42:18 +0900

    feat: E-TASK

--- train.js
+++ train.js
@@ -1,48 +1,65 @@
-/* D-TASK: 
+// E-TASK:
 
-Shunday class tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
-MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! */
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
 
-const moment = require("moment");
-
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-    this.time = moment().format("HH:mm");
+const getReverse = (str) => {
+  if (typeof str === "string") {
+    return [...str].reverse().join("");
+  } else {
+    console.log("Please, enter string");
   }
+};
 
-  qoldiq() {
-    console.log(
-      `Hozir ${this.time} da ${this.non} ta non, ${this.lagmon} ta lag'mon va ${this.cola} ta cola mavjud.`
-    );
-  }
+const result = getReverse("Hello World");
 
-  sotish(item, amount) {
-    if (this[item] >= amount) {
-      this[item] -= amount;
-      console.log(
-        `Hozir ${this.time} da ${this.non} ta non, ${this.lagmon} ta lag'mon va ${this.cola} ta cola mavjud. `
-      );
-    } else {
-      console.log(
-        `Hozir ${this.time} da ${amount} ta ${item} sotish imkoni yoq, yetarli emas.`
-      );
-    }
-  }
+console.log("Result:", result);
 
-  qabul(item, amount) {
-    this[item] += amount;
-    console.log(`Hozir ${this.time} da ${amount} ta ${item} qabul qilindi.`);
-  }
-}
+// /* D-TASK:
 
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-shop.qoldiq();
+// Shunday class tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! */
+
+// const moment = require("moment");
+
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//     this.time = moment().format("HH:mm");
+//   }
+
+//   qoldiq() {
+//     console.log(
+//       `Hozir ${this.time} da ${this.non} ta non, ${this.lagmon} ta lag'mon va ${this.cola} ta cola mavjud.`
+//     );
+//   }
+
+//   sotish(item, amount) {
+//     if (this[item] >= amount) {
+//       this[item] -= amount;
+//       console.log(
+//         `Hozir ${this.time} da ${this.non} ta non, ${this.lagmon} ta lag'mon va ${this.cola} ta cola mavjud. `
+//       );
+//     } else {
+//       console.log(
+//         `Hozir ${this.time} da ${amount} ta ${item} sotish imkoni yoq, yetarli emas.`
+//       );
+//     }
+//   }
+
+//   qabul(item, amount) {
+//     this[item] += amount;
+//     console.log(`Hozir ${this.time} da ${amount} ta ${item} qabul qilindi.`);
+//   }
+// }
+
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq();
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
+// shop.qoldiq();
 
 // C-TASK:
 
